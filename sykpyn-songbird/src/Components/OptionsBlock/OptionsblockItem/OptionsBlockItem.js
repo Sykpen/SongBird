64c4e3d1e1153c29f60c13a071a00faf9558af49
Sykpen <ser10potap@yandex.ru>
2020-08-04 18:28:58 +0300

    fix: change OptionsBlockItem from function to class component

--- sykpyn-songbird/src/Components/OptionsBlock/OptionsblockItem/OptionsBlockItem.js
+++ sykpyn-songbird/src/Components/OptionsBlock/OptionsblockItem/OptionsBlockItem.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const OptionsBlockItem = (props) => {
-  return (
-    <div className="options_block_main_item">
-      <div className="circle"></div>
-      <div className="options_block_main_item_bird">{props.BirdName}</div>
-    </div>
-  );
-};
+class OptionsBlockItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      isToggleOn: !state.isToggleOn,
+    }));
+  }
+  render() {
+    const { BirdName } = this.props;
+    return (
+      <div className="options_block_main_item">
+        <div className={this.state.isToggleOn ? "circle" : "circle red"}></div>
+        <div className="options_block_main_item_bird">{BirdName}</div>
+      </div>
+    );
+  }
+}
 
 export default OptionsBlockItem;
